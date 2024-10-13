f8a03039dc86f056d39765b1dc4f5ea16b976e10
share121 <2854631158@qq.com>
2024-10-13 11:22:34 +0800

    f

--- ai.ts
+++ ai.ts
@@ -1,8 +1,11 @@
 import ollama, { Message } from "ollama";
 import { Mutex } from "async-mutex";
+import { spawn } from "child_process";
 
 const constraints = "\n\n约束：不包含借口或上下文，只返回答案。";
-const models = ollama.list();
+try {
+  spawn("ollama", ["serve"]);
+} catch {}
 
 export function createAi({
   model,
@@ -15,17 +18,23 @@ export function createAi({
 }) {
   let messages: Message[] = [];
   const aiMutex = new Mutex();
+
   async function* ai(prompt: string) {
     let len = messages.length - historyLength * 2;
     if (len > 0) messages = messages.slice(len);
     const msg = prompt + constraints;
     messages.push({ role: "user", content: msg });
-    const m = await models;
-    const modelInfo = m.models.find((m) => m.name === model);
-    if (modelInfo === undefined) {
-      throw new Error(
-        `你还没有安装 ${model} 模型，你可以使用 \`ollama pull ${model}\` 来安装`
-      );
+    try {
+      const models = await ollama.list();
+      const modelInfo = models.models.find((m) => m.name === model);
+      if (modelInfo === undefined) {
+        throw new Error(
+          `你还没有安装 ${model} 模型，你可以使用 \`ollama pull ${model}\` 来安装`
+        );
+      }
+    } catch {
+      spawn("ollama", ["serve"]);
+      throw new Error("ollama 未启动，已为你自动启动 ollama");
     }
     const stream = await ollama.chat({
       messages: [...globalMsg, ...messages],
